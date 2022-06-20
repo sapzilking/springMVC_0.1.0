2e2bb9a6d79a0e80e2c133aac88ed116c88fe36b
sugil <privatesugil@gmail.com>
2022-06-20 18:08:36 +0900

    스프링 MVC - 컨트롤러 통합

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Map;
 
--- src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
+++ src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -1,22 +1,29 @@
-package hello.servlet.web.springmvc.v1;
+package hello.servlet.web.springmvc.v2;
+
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
-import hello.servlet.web.frontcontroller.ModelView;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Controller
-public class SpringMemberSaveControllerV1 {
+@RequestMapping("springmvc/v2/members")
+public class SpringMemberControllerV2 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/springmvc/v1/members/save")
-    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+    @RequestMapping("/new-form")
+    public ModelAndView newForm() {
+        return new ModelAndView("new-form");
+    }
+
+    @RequestMapping("/save")
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
         String username = request.getParameter(("username"));
         int age = Integer.parseInt(request.getParameter("age"));
 
@@ -25,6 +32,16 @@ public class SpringMemberSaveControllerV1 {
 
         ModelAndView mv = new ModelAndView("save-result");
         mv.addObject("member", member);
+
+        return mv;
+    }
+
+    @RequestMapping
+    public ModelAndView members() {
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
 
         return mv;
     }
